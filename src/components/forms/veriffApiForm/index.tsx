ecce5813dee09eccf5b4cbbc088dda0d5c93f2f4
Reimann Robert <reimannrobert707@gmail.com>
2022-12-28 23:37:32 +0200

    Fetching questions, yet to display and figure out errors

--- src/components/forms/veriffApiForm/index.tsx
+++ src/components/forms/veriffApiForm/index.tsx
@@ -1,10 +1,9 @@
 "use client"
-import { Button, Stack } from "@mui/material"
+import { Button, LinearProgress, Stack } from "@mui/material"
 import { Form, Formik } from "formik"
 import _ from "lodash"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
-import { fetchChecks } from "src/services/veriff/lib/veriffService"
+import { useState } from "react"
 import { Result } from "src/services/veriff/type"
 import * as yup from "yup"
 import { Question, YesNo } from "../../../services/veriff/type/question"
@@ -16,17 +15,21 @@ export interface FormikResult {
 
 export const VeriffApiForm = () => {
   const router = useRouter()
-  const [questions, setQuestions] = useState<Question[]>()
+  const [questions, setQuestions] = useState<Question[]>([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const questionsResponse = await fetchChecks()
-      setQuestions(questionsResponse)
-    }
-    fetchData()
-  }, [setQuestions])
+  /* useEffect(() => {
+    fetchChecks()
+      .then((data) => {
+        console.log("questions", data)
+        setQuestions(data)
+      })
+      .catch((data) => {
+        console.error(data)
+      })
+  }, [setQuestions]) */
 
   const handleSubmit = ({ results }: FormikResult) => {
+    //TODO Use the endpoint to submit the form!
     console.log(
       "resultsLodash",
       _.takeWhile(results, (_, index) => results[index - 1]?.result === YesNo.YES || index === 0)
@@ -36,51 +39,55 @@ export const VeriffApiForm = () => {
 
   return (
     <>
-      {!!questions && (
-        <Formik
-          validateOnMount
-          initialValues={{
-            results: questions.map((question) => ({
-              checkId: question.id,
-              result: undefined
-            }))
-          }}
-          onSubmit={handleSubmit}
-          validationSchema={yup.object({
-            results: yup
-              .array()
-              .test(
-                "allYesOrOneNo",
-                (results) =>
-                  results?.every((result) => result.result === YesNo.YES) ||
-                  results?.some((result) => result.result === YesNo.NO) ||
-                  false
-              )
-          })}
-        >
-          {({ submitForm, isSubmitting, isValid, values }) => (
-            <Form>
-              <Stack direction="column" gap={1}>
+      {questions.length > 0 ? (
+        <>
+          <Formik
+            validateOnMount
+            initialValues={{
+              results: questions.map((question) => ({
+                checkId: question.id,
+                result: undefined
+              }))
+            }}
+            onSubmit={handleSubmit}
+            validationSchema={yup.object({
+              results: yup
+                .array()
+                .test(
+                  "allYesOrOneNo",
+                  (results) =>
+                    results?.every((result) => result.result === YesNo.YES) ||
+                    results?.some((result) => result.result === YesNo.NO) ||
+                    false
+                )
+            })}
+          >
+            {({ submitForm, isSubmitting, isValid, values }) => (
+              <Form>
                 <Stack direction="column" gap={1}>
-                  <QuestionsField
-                    questions={_.takeWhile(
-                      questions,
-                      (_, index) => values.results[index - 1]?.result === YesNo.YES || index === 0
-                    )}
-                  />
-                </Stack>
+                  <Stack direction="column" gap={1}>
+                    <QuestionsField
+                      questions={_.takeWhile(
+                        questions,
+                        (_, index) => values.results[index - 1]?.result === YesNo.YES || index === 0
+                      )}
+                    />
+                  </Stack>
 
-                <Button
-                  variant="contained"
-                  onClick={submitForm}
-                  disabled={isSubmitting || !isValid}
-                >
-                  Submit
-                </Button>
-              </Stack>
-            </Form>
-          )}
-        </Formik>
+                  <Button
+                    variant="contained"
+                    onClick={submitForm}
+                    disabled={isSubmitting || !isValid}
+                  >
+                    Submit
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </>
+      ) : (
+        <LinearProgress />
       )}
     </>
   )
